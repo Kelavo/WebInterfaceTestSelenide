1478c699404f07b9b3d36e58cafaee44664df7ce
kelavo <diak0n@mail.ru>
2022-11-02 22:17:49 +0300

    NewTest

--- src/test/java/netology/OrderingACardTest.java
+++ src/test/java/netology/OrderingACardTest.java
@@ -2,18 +2,23 @@ package netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class OrderingACard {
+public class OrderingACardTest {
+
+    @BeforeEach
+    void openURL() {
+        open("http://localhost:9999/");
+    }
 
     @Test
     void shouldSendARequest() {
-        open("http://localhost:9999/");
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Тарас Бульба");
+        form.$("[data-test-id=name] input").setValue("Тарас Бульба-Гоголь");
         form.$("[data-test-id=phone] input").setValue("+79781234567");
         form.$("[data-test-id=agreement]").click();
         form.$("[type=button]").click();
@@ -21,13 +26,41 @@ public class OrderingACard {
     }
 
     @Test
-    void shouldSendARequestMistake() {
-        open("http://localhost:9999");
+    void shouldSendARequestMistakeName() {
         SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Ivan Ivanov");
         form.$("[data-test-id=phone] input").setValue("+79781234567");
         form.$("[data-test-id=agreement]").click();
         form.$("[type=button]").click();
-        $("[class=input__sub]").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSendARequestMistakePhone() {
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Тарас Бульба-Гоголь");
+        form.$("[data-test-id=phone] input").setValue("-79781234567");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldSendARequestMistakeEmptyValue() {
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSendARequestMistakeWithoutACheckbox() {
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Тарас Бульба-Гоголь");
+        form.$("[data-test-id=phone] input").setValue("+79781234567");
+        form.$("[type=button]").click();
+        $("[class=checkbox__text]").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
